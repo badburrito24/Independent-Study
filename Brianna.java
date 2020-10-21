01ab1039bde76ba9a577c742fae82184f375dd1e
badburrito24 <59742387+badburrito24@users.noreply.github.com>
2020-10-20 23:40:27 -0500

    Add files via upload

---
+++ Brianna.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Brianna {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File brianna = new File("D:\\Eclipse\\eclipse_workspace\\UIL Scanner Labs\\src\\brianna.dat");
+		Scanner file = new Scanner(brianna);
+		int min = file.nextInt();
+		int max = file.nextInt();
+		int range = 0;
+		while (file.hasNextInt()) {
+			int current = file.nextInt();
+			if (current < min)
+				min = current;
+			if (current > max)
+				max = current;
+		}
+		System.out.println(min + " " + max + " " + (max-min));
+
+	}
+
+}
