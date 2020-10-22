445694f2b1a050b94d96a77bcce89244f86be506
badburrito24 <59742387+badburrito24@users.noreply.github.com>
2020-10-22 01:04:21 -0500

    Add files via upload

--- Deepa.java
+++ Deepa.java
@@ -1,4 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Deepa {
 
-}
+	public static void main(String[] args) throws FileNotFoundException {
+		File deepa = new File("./deepa.dat");
+		Scanner file = new Scanner(deepa);
+
+		int cases = file.nextInt();
+		int brackets1 = file.nextInt();
+		int[] caseOne = new int[brackets1];
+		
+		for(int i = 0; i < cases; i++) {
+			
+			
+			
+			
+			
+			
+		}
+		
+	    public double calcTax()
+	
+		
+		
+		/* for (int i = 1; file.hasNextInt(); i++) {
+			caseOneNumBrackets = file.nextInt();
+			if (i == (1 + caseOneNumBrackets)) {
+				for (int a = 1; a <= (1 + caseOneNumBrackets); a++)
+					caseTwoNumBrackets = file.nextInt();
+			}
+        
+			use 3 backticks instead of 1
+			
+			 if(i =
+			 int bracket = file.nextInt();
+			 int income = file.nextInt();   
+			 
+			 
+			 an array or two will make your life easier
+			 
+             and making a method to apply the brackets is a good idea
+			 */
+
+		}
+
+	}
+
