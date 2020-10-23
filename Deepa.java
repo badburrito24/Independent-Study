ce7b49e99814c5686f2178a193e68cffd7f82bfe
badburrito24 <59742387+badburrito24@users.noreply.github.com>
2020-10-23 01:40:01 -0500

    Add files via upload

--- Deepa.java
+++ Deepa.java
@@ -9,42 +9,39 @@ public class Deepa {
 		Scanner file = new Scanner(deepa);
 
 		int cases = file.nextInt();
-		int brackets1 = file.nextInt();
-		int[] caseOne = new int[brackets1];
-		
-		for(int i = 0; i < cases; i++) {
-			
-			
-			
-			
-			
-			
-		}
-		
-	    public double calcTax()
-	
-		
-		
-		/* for (int i = 1; file.hasNextInt(); i++) {
-			caseOneNumBrackets = file.nextInt();
-			if (i == (1 + caseOneNumBrackets)) {
-				for (int a = 1; a <= (1 + caseOneNumBrackets); a++)
-					caseTwoNumBrackets = file.nextInt();
+
+		for (int i = 0; i < cases; i++) {
+			int bracketCount = file.nextInt();
+			int income = file.nextInt();
+
+			// arrays
+			int[] amounts = new int[bracketCount];
+			int[] percents = new int[bracketCount];
+
+			for (int j = 0; j < bracketCount; j++) {
+				amounts[i] = file.nextInt();
+				percents[i] = file.nextInt();
+
 			}
-        
-			use 3 backticks instead of 1
-			
-			 if(i =
-			 int bracket = file.nextInt();
-			 int income = file.nextInt();   
-			 
-			 
-			 an array or two will make your life easier
-			 
-             and making a method to apply the brackets is a good idea
-			 */
+			calcTax(amounts, percents, income);
 
 		}
-
 	}
 
+	public static double calcTax(int[] amount_array, int[] percent_array, int balance) {
+		double tax = 0;
+		for (int i = 0; i < amount_array.length - 1; i++) {
+			if ((i + 1) != amount_array.length) {
+				double taxableAmount = Math.min((amount_array[i + 1] - amount_array[i]), balance);
+				tax += (taxableAmount * (percent_array[i] / 100));
+				balance -= taxableAmount;
+			} else {
+				tax += (balance * (percent_array[i] / 100));
+			}
+		}
+
+		System.out.println(tax);
+		return tax;
+
+	}
+}
