90754f45b57f15d9e4df9ab110f4290e01e1778f
badburrito24 <59742387+badburrito24@users.noreply.github.com>
2020-10-23 12:52:25 -0500

    Add files via upload

--- Deepa.java
+++ Deepa.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Deepa {
@@ -7,41 +8,50 @@ public class Deepa {
 	public static void main(String[] args) throws FileNotFoundException {
 		File deepa = new File("./deepa.dat");
 		Scanner file = new Scanner(deepa);
-
+		double taxRate = 0;
 		int cases = file.nextInt();
+		file.nextLine();
 
 		for (int i = 0; i < cases; i++) {
 			int bracketCount = file.nextInt();
-			int income = file.nextInt();
+			double income = file.nextInt();
+			file.nextLine();
 
 			// arrays
-			int[] amounts = new int[bracketCount];
-			int[] percents = new int[bracketCount];
+			double[] amounts = new double[bracketCount];
+			double[] percents = new double[bracketCount];
 
 			for (int j = 0; j < bracketCount; j++) {
-				amounts[i] = file.nextInt();
-				percents[i] = file.nextInt();
-
+				amounts[j] = file.nextInt();
+				percents[j] = file.nextInt();
+				file.nextLine();
 			}
-			calcTax(amounts, percents, income);
+			//System.out.println(Arrays.toString(amounts));
+			//System.out.println(Arrays.toString(percents));
+			taxRate = ((calcTax(amounts, percents, income)) / income) * 100;
+			System.out.println("Case #" + (i + 1) + ": " + calcTax(amounts, percents, income) + " " + taxRate);
 
 		}
 	}
 
-	public static double calcTax(int[] amount_array, int[] percent_array, int balance) {
+	public static double calcTax(double[] amount_array, double[] percent_array, double balance) {
 		double tax = 0;
-		for (int i = 0; i < amount_array.length - 1; i++) {
-			if ((i + 1) != amount_array.length) {
-				double taxableAmount = Math.min((amount_array[i + 1] - amount_array[i]), balance);
-				tax += (taxableAmount * (percent_array[i] / 100));
-				balance -= taxableAmount;
-			} else {
-				tax += (balance * (percent_array[i] / 100));
-			}
-		}
+		double taxableAmount = 0;
 
-		System.out.println(tax);
+		for (int i = 0; i < amount_array.length - 1; i++) {
+			double percent = percent_array[i];
+			taxableAmount = Math.min((amount_array[i + 1] - amount_array[i]), balance);
+			tax += (taxableAmount * (percent / 100.0));
+			balance -= taxableAmount;
+
+		}
+		double percent = percent_array[percent_array.length - 1];
+		tax += (balance * (percent / 100));
+
+		// System.out.println(tax);
+
 		return tax;
 
 	}
+
 }
